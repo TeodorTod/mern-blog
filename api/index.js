cb38d05ce8551c0501487c0ac968b819dee0b524
TeodorTod <teodor.at.tod@gmail.com>
2024-09-14 22:31:34 +0300

    Mongo connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_STRING)
+    .then(() => {
+        console.log("DB connected successfuly!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-
+    console.log("Server is running on port 8080");
 });
