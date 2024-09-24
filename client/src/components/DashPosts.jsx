fd7b435408ee311ab7646d18703b3c33da38636f
TeodorTod <teodor.at.tod@gmail.com>
2024-09-24 21:44:34 +0300

    get posts

---
+++ client/src/components/DashPosts.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { useSelector } from 'react-redux';
+
+const DashPosts = () => {
+    const { currentUser } = useSelector((state) => state.user);
+    const [userPosts, setUserPosts] = useState([]); console.log(userPosts);
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+                const data = await res.json();
+                if (res.ok) {
+                    setUserPosts(data.posts)
+                }
+            } catch (error) {
+
+            }
+        };
+        if (currentUser.isAdmin) {
+            fetchPosts();
+        }
+    }, [currentUser._id]);
+
+    return (
+        <div>DashPosts</div>
+    )
+}
+
+export default DashPosts
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,19 +1,19 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from 'react-icons/hi'
-import { useDispatch } from "react-redux";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
 
-
 const DashSidebar = () => {
-    const [tab, setTab] = useState('');
+    const [tab, setTab] = useState("");
     const location = useLocation();
     const dispatch = useDispatch();
+    const { currentUser } = useSelector((state) => state.user);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
+        const tabFromUrl = urlParams.get("tab");
         if (tabFromUrl) {
             setTab(tabFromUrl);
         }
@@ -38,19 +38,41 @@ const DashSidebar = () => {
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
-                <Sidebar.ItemGroup>
-                    <Link to='/dashboard?tab=profile'>
-                        <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'user'} labelColor='dark' as='div'>
+                <Sidebar.ItemGroup className="flex flex-col gap-1">
+                    <Link to="/dashboard?tab=profile">
+                        <Sidebar.Item
+                            active={tab === "profile"}
+                            icon={HiUser}
+                            label={currentUser.isAdmin ? "admin" : "user"}
+                            labelColor="dark"
+                            as="div"
+                        >
                             Profile
                         </Sidebar.Item>
                     </Link>
-                    <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
+                    {currentUser.isAdmin && (
+                        <Link to="/dashboard?tab=posts">
+                            <Sidebar.Item
+                                active={tab === 'post'}
+                                icon={HiDocumentText}
+                                as="div"
+                            >
+                                Posts
+                            </Sidebar.Item>
+                        </Link>
+                    )}
+
+                    <Sidebar.Item
+                        icon={HiArrowSmRight}
+                        className="cursor-pointer"
+                        onClick={handleSignout}
+                    >
                         Sign Out
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
             </Sidebar.Items>
         </Sidebar>
-    )
-}
+    );
+};
 
-export default DashSidebar
+export default DashSidebar;
